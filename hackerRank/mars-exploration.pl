2264f6fd93f86c27e1001f9630b6581e3600dd90
Wojciech Kuchta <klapi85@gmail.com>
2016-08-17 23:43:02 +0200

    hackerRank start

---
+++ hackerRank/mars-exploration.pl
@@ -0,0 +1,25 @@
+use strict;
+
+## W. Kuchta 2016
+
+my $s = <STDIN>;
+
+my $sum = 0;
+
+while ($s =~ /(...)([A-Za-z0-9]*)/) {
+    $s = $2;
+    my $result = checkTriple($1);
+    $sum += $result;
+}
+print $sum;
+
+sub checkTriple() {
+        my ($t) = @_;
+        my $result = 0;
+
+        if ('S' ne substr($t, 0, 1)) { $result++; }
+        if ('O' ne substr($t, 1, 1)) { $result++; }
+        if ('S' ne substr($t, 2, 1)) { $result++; }
+
+        return $result;
+}
