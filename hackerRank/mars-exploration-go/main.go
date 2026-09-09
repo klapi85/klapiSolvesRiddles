e2c556af14cd4c2dfd6498f5e40436997bcbff7f
Wojciech Kuchta <klapi85@gmail.com>
2026-09-09 09:17:40 +0200

    mars-exploration in Go - small refactor

--- hackerRank/mars-exploration-go/main.go
+++ hackerRank/mars-exploration-go/main.go
@@ -10,18 +10,21 @@ import (
  *
  */
 
+func checkLetter(so string, letter string) int {
+	result := 0
+
+	if so != letter {
+		result++
+	}
+	return result
+}
+
 func marsExploration(s string) int {
 	result := 0
 	for i := 0; i < len(s); i += 3 {
-		if "S" != s[i:i+1] {
-			result++
-		}
-		if "O" != s[i+1:i+2] {
-			result++
-		}
-		if "S" != s[i+2:i+3] {
-			result++
-		}
+		result += checkLetter("S", s[i:i+1])
+		result += checkLetter("O", s[i+1:i+2])
+		result += checkLetter("S", s[i+2:i+3])
 	}
 	return result
 }
