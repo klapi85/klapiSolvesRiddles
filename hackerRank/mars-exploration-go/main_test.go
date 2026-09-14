df97208b7ca7ba3b96723b009c4b871960f8a25f
Wojciech Kuchta <klapi85@gmail.com>
2026-09-14 15:10:37 +0200

    mars-exploration in Go - more test cases in Go

--- hackerRank/mars-exploration-go/main_test.go
+++ hackerRank/mars-exploration-go/main_test.go
@@ -3,11 +3,23 @@ package main
 import "testing"
 
 func TestCheckLetter(t *testing.T) {
-	got := checkLetter("S", "S")
-	want := 0
 
-	if got != want {
-		// t.Errorf zgłasza błąd i kontynuuje wykonywanie testu
-		t.Errorf("checkLetter(\"S\", \"S\") = %d; expected %d", got, want)
+	tests := []struct {
+		name     string
+		a, b     string
+		expected int
+	}{
+		{name: "Comparing 2xS", a: "S", b: "S", expected: 0},
+		{name: "Comparing 2xO", a: "O", b: "O", expected: 0},
+		{name: "Comparing S&O", a: "S", b: "O", expected: 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := checkLetter(tt.a, tt.b)
+			if got != tt.expected {
+				t.Errorf("checkLetter(\"%s\", \"%s\") = %d; expected %d", tt.a, tt.b, got, tt.expected)
+			}
+		})
 	}
 }
